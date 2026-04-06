2712e0f2a16623725878331ad9d468f04e6111bf
Pavel Rudachikhin <pv.rd@yandex.com>
2026-04-06 10:13:09 +0300

    dig led

---
+++ get-led/digital-blink.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as gp
+import time
+gp.setmode(gp.BCM)
+led = 26
+gp.setup(led, gp.OUT)
+st = 0
+per = 1.0
+while True:
+    gp.output(led, st)
+    st = not st
+    time.sleep(per)
