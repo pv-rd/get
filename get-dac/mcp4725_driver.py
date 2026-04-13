fbbbd8a130b7a17147601b3d7daa6dab0fda8b1c
Pavel Rudachikhin <pv.rd@yandex.com>
2026-04-13 11:33:09 +0300

    i2c good

--- get-dac/mcp4725_driver.py
+++ get-dac/mcp4725_driver.py
@@ -1,9 +1,12 @@
 import RPi.GPIO as gp
 import smbus
+from dec2bin import *
 
 class MCP4725:
     def __init__(self, dynamic_range, address =0x61, verbose = True):
         self.bus = smbus.SMBus(1)
+
+        self.address = address
         self.wm = 0x00
         self.pds = 0x00
 
@@ -30,17 +33,28 @@ class MCP4725:
             if self.verbose:
                 print(f"Число: {number}, отправленые по I2C данные: [0x{(self.address << 1):02X}, 0x{first_byte:02X}, 0x{second_byte:02X}]\n")
 
+    @staticmethod
+    def voltage_to_number(voltage):
+        if not (0.0 <= voltage <= dynamic_range):
+            print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {dynamic_range:.2f}) В")
+            print("Устанавливаем 0.0 В")
+            return 0
+        return int(voltage / dynamic_range * (2**12-1))
+    
+    def set_voltage(self, voltage):
+        self.set_number(self.voltage_to_number(voltage))
+
+
 
         
         
 
-dynamic_range = 3.3
-pin = 12
-f = 200
+dynamic_range = 5
+
 
 if __name__ == "__main__":
     try:
-        dac = PWM_DAC(12, 500, dynamic_range, True)
+        dac = MCP4725(dynamic_range)
 
         while True:
             try:
